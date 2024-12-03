282775e7979447faa224cfa6a281a8c78e08385b
kimeming <kimeming030@gmail.com>
2024-12-03 17:39:19 +0900

    서브페이지 완료, 레시피 데이터 js 분리해둠

--- project01/source/js/sub.js
+++ project01/source/js/sub.js
@@ -6,37 +6,46 @@ $(document).ready(function () {
     $popupCloseBtn = $(".popup .close-btn"),
     $recipeBtn = $(".recipe-grid .link-btn");
 
-  $recipeBtn.on("click", function () {
+  // popup show, close function
+  function popShow() {
     $popupWrap.addClass("on");
     $body.addClass("on");
     $dimm.addClass("on");
+  }
+
+  function popClose() {
+    $popupWrap.removeClass("on");
+    $body.removeClass("on");
+    $dimm.removeClass("on");
+  }
+
+  $recipeBtn.on("click", function () {
+    popShow();
     let curRecipe = $(this).data("recipe");
     let Name = recipeArr[curRecipe][0].name,
       Src = recipeArr[curRecipe][0].src,
       Ingredient = recipeArr[curRecipe][0].ingredient,
       Recipe = recipeArr[curRecipe][0].recipe;
 
-      // Ingredient 가공
-      Ingredient = Ingredient.split(" | ");
+    // Ingredient 가공
+    Ingredient = Ingredient.split(" | ");
 
-      // map(v=>가공된v값)
-      // =>변수에 할당시 새로운 배열값으로 같은 배열주소에 재할당함
-      // -> 원본배열은 보존한다!
-      let splitIng = Ingredient.map(v=>`<span>${v}</span>`);
-      // console.log(temp);
+    // map(v=>가공된v값)
+    // =>변수에 할당시 새로운 배열값으로 같은 배열주소에 재할당함
+    // -> 원본배열은 보존한다!
+    let splitIng = Ingredient.map((v) => `<span>${v}</span>`);
 
-      // forEach 사용하는 방법
-      // let temp = [];
+    // forEach 사용하는 방법
+    // let temp = [];
 
-      // Ingredient.forEach((v,i)=>{
-      //   temp[i] = `<aside>${v}</aside>`;
-      // }); ///// forEach ////
+    // Ingredient.forEach((v,i)=>{
+    //   temp[i] = `<aside>${v}</aside>`;
+    // }); ///// forEach ////
 
-      // recipe 가공
-      Recipe = Recipe.split(" | ");
+    // recipe 가공
+    Recipe = Recipe.split(" | ");
 
-      let splitRecipe = Recipe.map(v => `<p>${v}</p>`);
-
+    let splitRecipe = Recipe.map((v) => `<p>${v}</p>`);
 
     $popupWrap.find(".title").text(Name);
     $popupWrap.find("img").attr("src", `/project01/source/img/sub/${Src}`);
@@ -45,34 +54,140 @@ $(document).ready(function () {
     $popupWrap.find(".recipe").html(splitRecipe);
   });
 
+  // popup close js
   $popupCloseBtn.on("click", function () {
-    $popupWrap.removeClass("on");
-    $body.removeClass("on");
-    $dimm.removeClass("on");
+    popClose();
   });
 
-  // 레시피 내용 변경
-  const recipeArr = 
-  {
+  // 외부 영역 클릭시 팝업 닫기
+  $(document).mouseup(function (e) {
+    if ($popupWrap.has(e.target).length === 0) {
+      popClose();
+    }
+  });
+
+  // 레시피 데이터 배열
+  const recipeArr = {
     bananaBear: [
       {
         name: "Banana Bear",
         src: "img-recipe01.jpg",
-        ingredient: `75 g butter, soft | 90 g powdered sugar and a little more for dusting | 1 egg | 2 bananas, ripe 80 g flour | z tsp baking powder | ¼ tsp cinnamon 1 banana | 20 g butter | 1 tbsp sugar 100 g HARIBO Goldbears /75 ml cream 2 Goldbear baking molds`,
+        ingredient: `75 g butter, soft | 90 g powdered sugar and a little more for dusting | 1 egg | 2 bananas, ripe 80 g flour | z tsp baking powder | ¼ tsp cinnamon 1 banana | 20 g butter | 1 tbsp sugar 100 g HARIBO Goldbears | 75 ml cream | 2 Goldbear baking molds`,
         recipe: `1. Preheat the oven to 180 *C al top and boltom heat, | 
-        2. Mash the two ripe bananas in a bowl with a fork, In another bowl, beat the butter with the powdered sugar until creamy. | 
-        Then stir in the egg, the mashed bananas, the flour, the baking powder and the cinnamon. | 
-        3. Divide the batter evenly between the two Goldbear molds and put them in the oven for 30 minules. Allow the bears to cool completely before removing them from the mold. Take them out of the moid and dust with a little powdered sugar. | 
-        4. Now It's time for the decoration: peel the last banana and cut it in half lengthwise. Put the butter with the sugar into a pan and let it melt over medium heat. Put the banana halves into the pan and caramelize them for about 5 minutes. In the meantime you can whip the cream until stiff. | 
-        5. Decorate the bears with the banana halves, the whipped cream and the Goldbears: Have fun with your banana bears!`,
+          2. Mash the two ripe bananas in a bowl with a fork, In another bowl, beat the butter with the powdered sugar until creamy. | 
+          Then stir in the egg, the mashed bananas, the flour, the baking powder and the cinnamon. | 
+          3. Divide the batter evenly between the two Goldbear molds and put them in the oven for 30 minules. Allow the bears to cool completely before removing them from the mold. Take them out of the moid and dust with a little powdered sugar. | 
+          4. Now It's time for the decoration: peel the last banana and cut it in half lengthwise. Put the butter with the sugar into a pan and let it melt over medium heat. Put the banana halves into the pan and caramelize them for about 5 minutes. In the meantime you can whip the cream until stiff. | 
+          5. Decorate the bears with the banana halves, the whipped cream and the Goldbears: Have fun with your banana bears!`,
       },
     ],
     brownBear: [
       {
         name: "Brown Bear",
         src: "img-recipe02.jpg",
-        ingredient: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae officia nulla nihil? Ab voluptas dolorum quibusdam laboriosam doloremque harum blanditiis assumenda vero recusandae illum! Officiis provident veritatis illo quaerat! Id!`,
-        recipe: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae officia nulla nihil? Ab voluptas dolorum quibusdam laboriosam doloremque harum blanditiis assumenda vero recusandae illum! Officiis provident veritatis illo quaerat! Id!Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae officia nulla nihil? Ab voluptas dolorum quibusdam laboriosam doloremque harum blanditiis assumenda vero recusandae illum! Officiis provident veritatis illo quaerat! Id!`,
+        ingredient: `
+            50 g dark chocolate | 50 g butter | 
+            90 g sugar | 1 sachet vanilla sugar | 1 egg | 
+            50 g flour | 1 pinch of salt | 50 g milk chocolate | 
+            1 Goldbear baking mold
+          `,
+        recipe: `
+            1. Preheat the oven to 180 °C. | 
+            2. Chop the dark chocolate coarsely and put it in a bowl with the butter. Melt both over a bain-marie. Remove the bowl from the heat and let the mixture cool down a little. Then, using a wooden spoon stir in the sugar, vanilla sugar and egg with a wooden spoon until everything is evenly mixed. Add the flour and salt. | 
+            3. Fill the Goldbear mold with the mixture and bake for 30 minutes. Leave to cool completely on a wire rack, then carefully remove the bear from the mold | 
+            4. Chop the milk chocolate coarsely and melt it in a bowl in a bain-marie. Now decorate the brown bear with the chocolate. Pour the melted chocolate into a small piping bag and draw in the eyes, mouth and nose. | 
+            It's ready to be eaten!
+          `,
+      },
+    ],
+    GingerBear: [
+      {
+        name: "Gingerbread Bear",
+        src: "img-recipe03.jpg",
+        ingredient: `
+              For the cake: | 
+              110 g granulated sugar | 110 g butter | 
+              1 large egg, beaten | 280 g molasses | 
+              140 g all-purpose flour | ½ teaspoon baking soda | 
+              1 teaspoon ground cinnamon | 1 teaspoon ground ginger | 
+              ½ teaspoon ground cloves | Va teaspoon salt 250 ml very hot water Bananas for topping | 
+              Fresh whipped cream for topping | 
+              For the vanilla cream sauce: | 
+              200 g granulated sugar | 110 g butter | 
+              240 g heavy whipping cream | 2 teaspoons vanilla extract | 
+            `,
+        recipe: `
+              1. Preheat the oven to 180°C (lop/bottom heat). Line the 18 cm-diameter springform tin with baking paper. Whisk the eggs with the sugar and the vanilla sugar for about 8 minutes until thick and creamy. Stir in the salt. | 
+              2. Combine the cornflour, baking powder and flour, then sift it over the egg mixture and fold it in. Transfer the mixture to the prepared cake lin and bake for 18 minutes, or until golden. Take out, remove form the tin and leave to cool on a wire | 
+              3. To make the fruit gum layer, line the two 16 cm-diameter springform tins with cling film. Separately, heat and dissolve the green and red Goldbears in 75 ml water each. Pour the liquids into the two springform tins and | 
+              4. Heal and melt the white Goldbears for the quark cream in 40 ml water. Stir the milk and the sugar into the quark until smooth. Add 2-3 ibsp of the quark mixture to the dissolved white Goldbears and stir in. Add the remaining quark midure. Beat the whipping cream until stiff, then sprinkle in the vanilla sugar. Stir into the quark mixture. | 
+              5. Halve the cake horizontally and place one half onto a cake platter. Surround with a cake ring, leaving 1 cm free all around. | 
+              6. Spread one-quarter of the cream onto the cake, letting it run into the gap between the cake and the cake ring. Remove the first Goldbear filing from the tin and place it on top of the cream. Cover with a second quarler of the cream. Place the second Goldbear filling on top and cover with another quarter of cream. Place the second cake on top of the cream and cover with the remaining cream. | 
+              7. Leave the cake to set in the fridge for at least 4 hours, ideally overnight. Before serving carefully remove the cake from the springform tin and decorate with Goldbears!       
+          `,
+      },
+    ],
+    SYPBear: [
+      {
+        name: "Strawberry Yogurt Polar Bear",
+        src: "img-recipe04.jpg",
+        ingredient: `75 g strawberries | 60 ml cream | 130 g Greek yogurt | 1 tablespoon honey | approx. 100 g of Goldbears | 1 Goldbear baking mold`,
+        recipe: `
+            1. Wash the strawberries and puree them with a hand | 
+            2. Whip the cream until stiff. Mix the Greek yogurt with the honey and then fold in the cream. | 
+            3. Spread 2 tablespoons of strawberry puree in the Goldbear mold, fill the yogurt mixture and add the remaining puree in blobs on top of the yogurt. Pull a fork through the mixture to give your bear a nice marbling | 
+            4. As a little surprise you can puf Goldbears on the yogurt. | 
+            This way you will have a Goldbears bottom under the ice | 
+            5. Let the mold freeze for at least 5 hours. Remove the bear from the mold, let it thaw briefly and enjoy the fruity-fresh ice cream bear!
+          `,
+      },
+    ],
+    goldbearsCake: [
+      {
+        name: "Goldbears Cake",
+        src: "img-recipe05.jpg",
+        ingredient: `
+            For the cake: | 
+            2 eggs | 50 g caster sugar | 1 sachet vanilla sugar a pinch of salt | 40 g cornflour | 1 teaspoon of baking powder | 60 g plain flour | 
+            For the fruit gum layers: | 
+            120 g each green and red HARIBO Goldbears | 
+            150 ml water | 
+            For the quark cream: | 
+            90 g white HARIBO Goldbears 40 ml water | 
+            125 ml milk | 1 tablespoon caster sugar | 
+            500 g low-fat quark | 400 ml whipping cream | 
+            2 sachets vanilla sugar | 
+            Also: | 
+            1 springform cake tin, 18cm  |  2 springform cake tins, • 16 cm | 
+            450 g HARIBO Goldbears | 
+          `,
+        recipe: `
+            1. Preheat the oven to 180°C (lop/bottom heat). Line the 18 cm-diameter springform tin with baking paper. Whisk the eggs with the sugar and the vanilla sugar for about 8 minutes until thick and creamy. Stir in the salt. | 
+            2. Combine the cornflour, baking powder and flour, then sift it over the egg mixture and fold it in. Transfer the mixture to the prepared cake lin and bake for 18 minutes, or until golden. Take out, remove form the tin and leave to cool on a wire | 
+            3. To make the fruit gum layer, line the two 16 cm-diameter springform tins with cling film. Separately, heat and dissolve the green and red Goldbears in 75 ml water each. Pour the liquids into the two springform tins and | 
+            4. Heal and melt the white Goldbears for the quark cream in 40 ml water. Stir the milk and the sugar into the quark until smooth. Add 2-3 ibsp of the quark mixture to the dissolved white Goldbears and stir in. Add the remaining quark midure. Beat the whipping cream until stiff, then sprinkle in the vanilla sugar. Stir into the quark mixture. | 
+            5. Halve the cake horizontally and place one half onto a cake platter. Surround with a cake ring, leaving 1 cm free all around. | 
+            6. Spread one-quarter of the cream onto the cake, letting it run into the gap between the cake and the cake ring. Remove the first Goldbear filing from the tin and place it on top of the cream. Cover with a second quarler of the cream. Place the second Goldbear filling on top and cover with another quarter of cream. Place the second cake on top of the cream and cover with the remaining cream. | 
+            7. Leave the cake to set in the fridge for at least 4 hours, ideally overnight. Before serving carefully remove the cake from the springform tin and decorate with Goldbears!
+          `,
+      },
+    ],
+    RCBear: [
+      {
+        name: "Raspberry Chocolate Bear",
+        src: "img-recipe06.jpg",
+        ingredient: `100 g raspberries | 20 g sugar | 1 sheet gelatine | 90 ml cream | 6 chocolate double biscuits | 25 g butter, melted | 1 Goldbear baking mold`,
+        recipe: `
+          1. Wash the raspberries and puree them with a hand blender.
+          Pass the puree through a sieve to remove the seeds. Soften the gelatine in a bowl of cold water for 5 minutes.
+          2. Boil the raspberry puree in a pan with the sugar and remove from the heat. Squeeze the gelatine well and stir it into the warm puree. Let the mixture cool to oom temperature. | 
+          3. In the meantime, whip the cream until stiff. Then carefully fold it into the puree and fill the mixture into the Goldbear mold. | 
+          Leave about 5 mm from the top of the edge. Put the mould in the freezer for 30 minutes. | 
+          4. To crumble the chocolate biscuits, put them in a freezer bag and roll over them with a dough roller. Mix the crumbs with the melted butter. | 
+          5. After 30 minutes, take the bear out of the freezer and spread the biscuit mixture evenly over the raspberry cream and freeze the bear again, this time for at least 2 hours. | 
+          6. Carefully press the bear out of the mold. | 
+          Now you need a little patience - the bear should thaw completely before you taste it. But then it's time: On your spoons, get set, go!
+          `,
       },
     ],
   };

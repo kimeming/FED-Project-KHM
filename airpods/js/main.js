9027ce909e135c91c242fde332641c1522a1453a
kimeming <kimeming030@gmail.com>
2024-12-13 17:22:21 +0900

    작업중

---
+++ airpods/js/main.js
@@ -0,0 +1,19 @@
+// layout.js
+$(() => {
+    // header
+
+    // footer fnb toggle js
+    const fnbTitle = $('.fnb-title'),
+        fnbItem = $('.fnb-item'),
+        fnbList = $('.fnb-list');
+    
+    fnbTitle.on('click', function(){
+        if(!$(this).parent(fnbItem).hasClass('active')){
+            fnbItem.removeClass('active');
+            $(this).parent(fnbItem).addClass('active');
+        } else {
+            $(this).parent(fnbItem).removeClass('active')
+        }
+    });
+    
+});
